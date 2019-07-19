62e52c8c8fa0b93f0d0510d7d1861cb40cea5eb7
Matthew Kim <m234kim@edu.uwaterloo.ca>
2019-07-18 21:17:45 -0400

    updated createRendezvue.sql to create database user, drop all tables, populate tables with test data

--- db-scripts/createRendezvue.sql
+++ db-scripts/createRendezvue.sql
@@ -1,3 +1,12 @@
+CREATE USER IF NOT EXISTS 'newuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT ALL PRIVILEGES ON * . * TO 'newuser'@'localhost';
+FLUSH PRIVILEGES;
+
+DROP TABLE IF EXISTS Locations;
+DROP TABLE IF EXISTS UserRoom;
+DROP TABLE IF EXISTS Rooms;
+DROP TABLE IF EXISTS Users;
+
 CREATE TABLE `Users` (
 	`userId` int(32) NOT NULL AUTO_INCREMENT,
 	`username` varchar(255) NOT NULL UNIQUE,
@@ -19,7 +28,7 @@ CREATE TABLE `Locations` (
 );
 
 CREATE TABLE `UserRoom` (
-	`roomId` int(32) NOT NULL AUTO_INCREMENT,
+	`roomId` int(32) NOT NULL,
 	`userId` int(32) NOT NULL,
 	PRIMARY KEY (`roomId`,`userId`)
 );
@@ -37,3 +46,33 @@ ALTER TABLE `Locations` ADD CONSTRAINT `Locations_fk1` FOREIGN KEY (`roomId`) RE
 ALTER TABLE `UserRoom` ADD CONSTRAINT `UserRoom_fk0` FOREIGN KEY (`roomId`) REFERENCES `Rooms`(`roomId`);
 
 ALTER TABLE `UserRoom` ADD CONSTRAINT `UserRoom_fk1` FOREIGN KEY (`userId`) REFERENCES `Users`(`userId`);
+
+INSERT INTO `Users` (
+  `userId`, `username`, `firstName`, `lastName`, `password`
+) VALUES (
+  1, 'fydp', 'rendezvue', 'RJTM', 'password'
+);
+
+INSERT INTO `Rooms` (
+	`roomId`, `roomName`
+) VALUES (
+	1, '202 Lester'
+), (
+	2, 'UWP'
+);
+
+INSERT INTO `UserRoom` (
+	`roomId`, `userId`
+) VALUES (
+	1, 1
+), (
+	2, 1
+);
+
+INSERT INTO `Locations` (
+	`locationId`, `userId`, `longitude`, `latitude`, `altitude`, `markerMetadata`, `roomId`
+) VALUES (
+	1, 1, -80.53306633319255, 43.46973120592646, 318, 'end of lester st', 1
+), (
+	2, 1, -80.53305430430403, 43.46972609896514, 319, 'UWP', 2
+);
